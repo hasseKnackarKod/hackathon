3622d339ff68af127c509266a581e5a82bb9a6b0
William Hansson <wi3542ha-s@student.lu.se>
2025-02-24 19:13:36 +0100

    Add pip install, wohoo lets go

---
+++ lincTest.py
@@ -0,0 +1,13 @@
+# Vi vill typ använda stoploss
+
+# API. Historiska data kan nås direkt
+# Samma dag, historisk data men andra aktier
+
+# Förra årets vinnare
+#       https://github.com/ErikLundb3rg/LINC-Hackathon-2024/blob/main/README.md 
+
+# Pip install hackathon-linc 
+import hackathon_linc as lh
+
+
+
