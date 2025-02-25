05e78c7f2bc4b1bb0e16fe1f558f88a69e13df84
William Hansson <wi3542ha-s@student.lu.se>
2025-02-25 19:09:45 +0100

    Kört alla funktioner en gång bara

--- lincTest.py
+++ lincTest.py
@@ -10,7 +10,47 @@
 
 # Pip install hackathon-linc 
 import hackathon_linc as lh
+import pandas as pd
+
+lh.init('92438482-5598-4e17-8b34-abe17aa8f598')
+
+# These are the Account functions
+orders = lh.get_all_orders()
+completed = lh.get_completed_orders()
+pending = lh.get_pending_orders()
+stoploss_orders = lh.get_stoploss_orders()
+balance = lh.get_balance()
+portfolio = lh.get_portfolio()
 
 
+print('Orders')
+print(orders)
+print('Completed')
+print(completed)
+print('Pending')
+print(pending)
+print('Stoploss')
+print(stoploss_orders)
+print('Balance')
+print(balance) 
+print('Portfolio')
+print(portfolio)
+
+# These are the Order functions
+
+buy_response = lh.buy('STOCK1', 10, 100) # Default köp på current price. Ganska rimligt att vi vill de?
+sell_response = lh.sell('STOCK1', 10, 110) # Default sälj på current price. Ganska rimligt att vi vill de?
+
+stoploss_response = lh.stoploss('STOCK1', 10, 90)
+
+cancel_response = lh.cancel(order_id='1234', ticker = 'STOCK1')
+
+## Market data functions
+tickers = lh.get_all_tickers()
+price_data = lh.get_current_price('STOCK1')
+
+print('Tickers')
+
+historical_data = lh.get_historical_data(30, 'STOCK1')
 
 
