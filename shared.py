604f339e06a2db00b0f2777dadcb7171faec587f
James <milton.ritzing@gmail.com>
2025-02-27 20:48:05 +0100

    Modified shared.py to allow work in notebook

--- shared.py
+++ shared.py
@@ -1,6 +1,15 @@
 from multiprocessing import Manager
 
-manager = Manager()
-shared_data = manager.dict()  # Shared dictionary accessible live
-shared_data['df'] = None
-shared_data['df_daily'] = None
+def get_shared_data():
+    """Creates a shared dictionary using multiprocessing Manager."""
+    manager = Manager()
+    shared_data = manager.dict() # Shared dictionary accessible live
+    shared_data["df"] = None  # Initialize your shared dataframe
+    shared_data['df_daily'] = None
+    return shared_data
+
+shared_data = get_shared_data()
+# manager = Manager()
+# shared_data = manager.dict()  
+# shared_data['df'] = None
+
